3c34153cc3f80f3149e44025e6fcd85b1c2bf073
Duru <derindagsd@gmail.com>
2023-02-16 19:07:24 +0300

    Created header file

--- NEWGAME/main.cpp
+++ NEWGAME/main.cpp
@@ -1,54 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Player.h"
 
 using namespace std;
 
-class Player
-{
-private:
-    // attributes
-    string name;
-    int health{100};
-    int bullets{7};
-
-public:
-    // methods
-    void talk(string);
-    bool is_dead()
-    {
-        if (health == 0)
-            return 1;
-    }
-
-    int attack_damage()
-    {
-        /* Function to randomly generate how much damage is done to the health of the other player */
-        srand(time(0));
-
-        // Generate a random number between 0 and 50
-        int attack_point = rand() % 51;
-
-        return attack_point;
-    }
-
-    int defense(int damage)
-    {
-        /* Function to defend player against attack */
-        srand(time(0));
-
-        // Generate a random number between 0 and the amount of damage from the attack
-        int defense_point = rand() % damage;
-
-        return defense_point;
-    }
-
-    void set_name(string n) { name = n; }
-    string get_name() { return name; }
-};
-
-void get_player_names(vector<Player> &team, int numOfPlayers, int teamNumber);
-void print_players(vector<Player> &team, int teamNumber);
+void get_player_names(std::vector<Player> &team, int numOfPlayers, int teamNumber);
+void print_players(std::vector<Player> &team, int teamNumber);
 int roll_dice();
 int print_roll_dice();
 
@@ -58,6 +16,7 @@ int main()
     Player two;
     Player three;
     Player four;
+
     vector<Player> team_one{one, two};
     vector<Player> team_two{three, four};
 
@@ -76,51 +35,4 @@ int main()
     /* THE GAME STARTS HERE */
 
     return 0;
-}
-
-void get_player_names(vector<Player> &team, int numOfPlayers, int teamNumber)
-{
-    string nameHolder;
-    cout << "TEAM " << teamNumber << endl;
-    for (int i = 0; i < numOfPlayers; i++) // TEAM ONE
-    {
-        cout << "Player " << i + 1 << " what is your name: ";
-        cin >> nameHolder;
-        team.at(i).set_name(nameHolder);
-    }
-}
-
-void print_players(vector<Player> &team, int teamNumber)
-{
-    cout << "Team " << teamNumber << ":" << endl;
-    cout << "1. " << team.at(0).get_name() << endl
-         << "2. " << team.at(1).get_name() << endl;
-}
-
-int roll_dice()
-{
-    srand(time(0));
-
-    // Generate a random number between 0 and 50
-    int dice = rand() % 13;
-
-    return dice;
-}
-
-int print_roll_dice()
-{
-    // This function decides who goes first in the game and returns the teams number.
-    int team1_dice = roll_dice(), team2_dice = roll_dice();
-
-    cout << "\nTEAM ONE DICE ROLL: \n"
-         << team1_dice << endl;
-
-    cout << "TEAM TWO DICE ROLL: \n"
-         << team2_dice << endl;
-
-    if (team1_dice > team2_dice)
-        return 1;
-
-    else
-        return 2;
 }
