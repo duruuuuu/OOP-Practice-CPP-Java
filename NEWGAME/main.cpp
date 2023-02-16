3e2fd408dec1ad8b87a40e4389d5c4f0b19156a9
Duru <derindagsd@gmail.com>
2023-02-16 14:48:53 +0300

    Add roll_dice function to decide who goes first

--- NEWGAME/main.cpp
+++ NEWGAME/main.cpp
@@ -9,7 +9,7 @@ class Player
 private:
     // attributes
     string name;
-    int health{5};
+    int health{100};
     int bullets{7};
 
 public:
@@ -20,14 +20,37 @@ public:
         if (health == 0)
             return 1;
     }
-    bool attack();
-    int attack_damage();
+
+    int attack_damage()
+    {
+        /* Function to randomly generate how much damage is done to the health of the other player */
+        srand(time(0));
+
+        // Generate a random number between 0 and 50
+        int attack_point = rand() % 51;
+
+        return attack_point;
+    }
+
+    int defense(int damage)
+    {
+        /* Function to defend player against attack */
+        srand(time(0));
+
+        // Generate a random number between 0 and the amount of damage from the attack
+        int defense_point = rand() % damage;
+
+        return defense_point;
+    }
+
     void set_name(string n) { name = n; }
     string get_name() { return name; }
 };
 
 void get_player_names(vector<Player> &team, int numOfPlayers, int teamNumber);
 void print_players(vector<Player> &team, int teamNumber);
+int roll_dice();
+int print_roll_dice();
 
 int main()
 {
@@ -44,6 +67,13 @@ int main()
     cout << "\nHere are your teams: " << endl;
     print_players(team_one, 1);
     print_players(team_two, 2);
+
+    cout << "\nEach team must roll the dice to decide who goes first.\n"
+         << endl;
+
+    int goes_first = print_roll_dice();
+
+    /* THE GAME STARTS HERE */
 
     return 0;
 }
@@ -65,4 +95,32 @@ void print_players(vector<Player> &team, int teamNumber)
     cout << "Team " << teamNumber << ":" << endl;
     cout << "1. " << team.at(0).get_name() << endl
          << "2. " << team.at(1).get_name() << endl;
+}
+
+int roll_dice()
+{
+    srand(time(0));
+
+    // Generate a random number between 0 and 50
+    int dice = rand() % 13;
+
+    return dice;
+}
+
+int print_roll_dice()
+{
+    // This function decides who goes first in the game and returns the teams number.
+    int team1_dice = roll_dice(), team2_dice = roll_dice();
+
+    cout << "\nTEAM ONE DICE ROLL: \n"
+         << team1_dice << endl;
+
+    cout << "TEAM TWO DICE ROLL: \n"
+         << team2_dice << endl;
+
+    if (team1_dice > team2_dice)
+        return 1;
+
+    else
+        return 2;
 }
