f169c97c954a50498ab3ce0cc77fc0a06bc5b8bb
Duru <derindagsd@gmail.com>
2023-02-16 14:21:19 +0300

    New function for r printing names of players

--- NEWGAME/main.cpp
+++ NEWGAME/main.cpp
@@ -15,13 +15,19 @@ private:
 public:
     // methods
     void talk(string);
-    bool is_dead();
-    int attack();
+    bool is_dead()
+    {
+        if (health == 0)
+            return 1;
+    }
+    bool attack();
+    int attack_damage();
     void set_name(string n) { name = n; }
     string get_name() { return name; }
 };
 
-void get_player_names(vector<Player> &team, int numOfPlayers);
+void get_player_names(vector<Player> &team, int numOfPlayers, int teamNumber);
+void print_players(vector<Player> &team, int teamNumber);
 
 int main()
 {
@@ -32,28 +38,31 @@ int main()
     vector<Player> team_one{one, two};
     vector<Player> team_two{three, four};
 
-    get_player_names(team_one, 2);
-    get_player_names(team_two, 2);
+    get_player_names(team_one, 2, 1);
+    get_player_names(team_two, 2, 2);
 
-    cout << "Here are your teams: " << endl;
-    cout << "Team one: " << endl;
-    cout << "1. " << team_one.at(0).get_name() << endl
-         << "2. " << team_one.at(1).get_name() << endl;
-
-    cout << "Team two: " << endl;
-    cout << "1. " << team_two.at(0).get_name() << endl
-         << "2. " << team_two.at(1).get_name() << endl;
+    cout << "\nHere are your teams: " << endl;
+    print_players(team_one, 1);
+    print_players(team_two, 2);
 
     return 0;
 }
 
-void get_player_names(vector<Player> &team, int numOfPlayers)
+void get_player_names(vector<Player> &team, int numOfPlayers, int teamNumber)
 {
     string nameHolder;
+    cout << "TEAM " << teamNumber << endl;
     for (int i = 0; i < numOfPlayers; i++) // TEAM ONE
     {
         cout << "Player " << i + 1 << " what is your name: ";
         cin >> nameHolder;
         team.at(i).set_name(nameHolder);
     }
+}
+
+void print_players(vector<Player> &team, int teamNumber)
+{
+    cout << "Team " << teamNumber << ":" << endl;
+    cout << "1. " << team.at(0).get_name() << endl
+         << "2. " << team.at(1).get_name() << endl;
 }
