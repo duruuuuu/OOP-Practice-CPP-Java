ec4b81ca65e6d99f419ad1ab49e334a9c5464d6b
Duru <derindagsd@gmail.com>
2023-02-23 16:10:43 +0300

    Wrote sample driver code

---
+++ OperatorOverloading/Challenge/main.cpp
@@ -0,0 +1,23 @@
+#include "Movies.h"
+#include "Movie.h"
+#include <string>
+#include <iostream>
+
+void add_movie(Movies &movieGroup, std::string name, std::string rating, long int watchAmount)
+{
+    if (movieGroup.add_movie(name, rating, watchAmount))
+    {
+        std::cout << name << "Added" << std::endl;
+    }
+}
+
+int main()
+{
+    Movies my_movies;
+
+    my_movies.display();
+
+    add_movie(my_movies, "The Holiday", "PG-13", 7);
+    add_movie(my_movies, "The Ugly Truth", "R", 1);
+    add_movie(my_movies, "21 Dresses", "PG-13", 3);
+}
